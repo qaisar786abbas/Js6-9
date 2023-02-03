8694b1b50bb65f9c70f18271bf0e7d9af00e4f58
Qaisar Abbas <122449496+qaisar786abbas@users.noreply.github.com>
2023-02-03 18:56:29 +0500

    added files

---
+++ Js 6-9/app.js
@@ -0,0 +1,100 @@
+// *******CHAPTER 06 TO 09
+
+// Question no 01
+// var num = 10;
+// num = ++num;
+// alert(num);
+// document.write("<h1>Result</h1>");
+// document.write("<h2>The value of num is 10</h2>");
+// document.write("<h2>---------------------------</h2>");
+
+// document.write("<h2>The value of ++num is 11</h2>");
+// document.write("<h2>Now value of num is 11</h2>");
+
+
+// document.write("<h2> The value of num is 11</h2>");
+// num = ++num;
+// alert(num);
+// document.write("<h2> Now value of num is 12</h2>");
+
+// num = --num;
+// alert(num);
+// document.write("<h2> The value of --num is 11</h2>");
+// document.write("<h2> Now the value of num is 11</h2>");
+
+// num = num--;
+// alert(num);
+// document.write("<h2> The value of num-- is 11</h2>");
+// num = --num;
+// alert(num);
+// document.write("<h2> Now the  value of num is 10</h2>");
+
+//Question no 2
+
+// var a = 2;
+// var b = 1;
+// var result = --a - --b + ++b + b--;
+//             //    1  -  0  +  1  +  1
+// alert(result);
+// document.write("<h2>value of a is 2 but after pre decrement value of a is 1 </h2>");
+// document.write("<h2>value of b is 1 but after pre decrement value of b is 0</h2>");
+// document.write("<h2>Now condition is 1 - 0 + 1 + 1</h2>");
+// document.write("<h2>So result is: 3</h2>");
+
+//Question no 3
+
+// var message = "Hello,";
+// var userName = "Qaisar!";
+// alert(message + userName);
+// document.write( "Hello," + "Qaisar!"  );
+
+//Question no 5
+// var num = prompt("Enter a number" , "5");
+// var x = num;
+// var result = x * num;
+// num = num * 5;
+// alert(num);
+// if(x == num){
+// alert(result);
+// document.write("<h2>5 * 1 = 5</h2>");
+// document.write("<h2>5 * 2 = 10</h2>");
+// document.write("<h2>5 * 3 = 15</h2>");
+// document.write("<h2>5 * 4 = 20</h2>");
+// document.write("<h2>5 * 5 = 25</h2>");
+// document.write("<h2>5 * 6 = 30</h2>");
+// document.write("<h2>5 * 7 = 35</h2>");
+// document.write("<h2>5 * 8 = 40</h2>");
+// document.write("<h2>5 * 9 = 45</h2>");
+// document.write("<h2>5 * 10 = 50</h2>");
+// }   
+
+//Question no 06
+// var subject1 = "english";
+// var subject2 = "urdu";
+// var subject3 = "math";
+// alert(subject1);
+// alert(subject2);
+// alert(subject3);
+// var subject1 = 100;
+// var subject2 = 100;
+// var subject3 = 100;
+// total = subject1 + subject2 +subject3;
+// alert(total);
+//b
+// var obtainMark1 = 54;
+// var obtainMark2 = 65;
+// var obtainMark3 = 67;
+// obtain = (obtainMark1 +  obtainMark2 +  obtainMark3);
+// alert(obtain);
+//c
+// document.write("<h1>Subject---TotalMark---ObtainMark---Percentage</h1>");
+// document.write("<h1>English--------100----------- 54 ---------- 54%</h1>");
+// document.write("<h1>Urdu-----------100----------- 65 ---------- 65%</h1>");
+// document.write("<h1>Math-----------100----------- 67 -----------67%</h1>");
+// document.write("<h1>-----------------------------------------------------</h1>");
+// document.write("<h1>Total----------300------------186 -----------62%</h1>");
+
+
+
+
+
